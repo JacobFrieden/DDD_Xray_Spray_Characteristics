63c4f5e87599854309282a0ddca71d1c3d0b6537
JacobFrieden <jfrieden@iastate.edu>
2024-04-27 23:07:48 -0500

    Update Classical CV Programs

--- data_proc.py
+++ data_proc.py
@@ -9,6 +9,7 @@ import cv2
 import os
 import numpy as np
 import tifffile as tf
+import tqdm
 
 def increase_contrast(image, clip_limit=8.0, grid_size=(15, 15)):
     # Convert the image to grayscale if it's not already
@@ -39,11 +40,6 @@ def rand_crop(img, mask):
     mask = cv2.dilate(cv2.erode(mask, np.ones((3,3))),np.ones((3,3)))
     return img, mask
 
-def rand_noise(img,mask):
-    noise = np.random.normal(0, 2.5, img.shape).astype(np.uint8)
-    img = cv2.add(img, noise)
-    return img, mask
-
 def rand_flip(img, mask):
     flip_code = np.random.choice([-1,0,1])
     return cv2.flip(img, flip_code), cv2.flip(mask, flip_code)
@@ -60,42 +56,51 @@ def generate_binary_mask(image):
     c2ibw = cv2.dilate(c2ibw, np.ones((7,7)))
     return c2ibw
 
-DATASOURCE = 'Data/Processed data for 512_608_x=-2.08_y=31.2_Ql=0.099_Qg=75_Qsw=75_fps=6000'
-TRAINING_DIR = 'Processed Data/train/'
-TESTING_DIR = 'Processed Data/test/'
-if len(os.listdir(TRAINING_DIR+'IMGs')) + len(os.listdir(TESTING_DIR+'IMGs')) == 0:
-    for file in os.listdir(DATASOURCE):
-        split = np.random.random()
-        img = cv2.imread(os.path.join(DATASOURCE, file), cv2.IMREAD_GRAYSCALE)
-        binary_mask = generate_binary_mask(img)
-        _, cc_mask = cv2.connectedComponents(binary_mask)
-        if split < 0.8:
-            # Training Data
-            cv2.imwrite(os.path.join(TRAINING_DIR,'IMGs',file[-10:-4]+'.png'), img)
-            cv2.imwrite(os.path.join(TRAINING_DIR,'masks',file[-10:-4]+'.png'), cc_mask)
-            i = 1
-            while i < 5:
-                aug = False
-                aug_img, aug_mask = img, binary_mask
-                if np.random.random() > 0.5:
-                    aug = True
-                    aug_img, aug_mask = rand_crop(aug_img, aug_mask)
-                if np.random.random() > 0.5:
-                    aug = True
-                    aug_img, aug_mask = rand_flip(aug_img, aug_mask)
-                if np.random.random() > 0.5:
-                    aug = True
-                    aug_img, aug_mask = rand_noise(aug_img, aug_mask)
-                if aug:
-                    _, aug_mask = cv2.connectedComponents(aug_mask)
-                    cv2.imwrite(os.path.join(TRAINING_DIR,'IMGs',f'{file[-10:-4]}_{i}.png'), aug_img)
-                    cv2.imwrite(os.path.join(TRAINING_DIR,'masks',f'{file[-10:-4]}_{i}.png'), aug_mask)
-                    i+=1
-        else:
-            # Testing Data
-            cv2.imwrite(os.path.join(TESTING_DIR,'IMGs',file[-10:-4]+'.png'), img)
-            cv2.imwrite(os.path.join(TESTING_DIR,'masks',file[-10:-4]+'.png'), cc_mask)
-else:
-    print(f'{len(os.listdir(TRAINING_DIR+"IMGs"))=}')
-    print(f'{len(os.listdir(TESTING_DIR+"IMGs"))=}')
+if __name__ == "__main__": 
+
+    DATASOURCE = 'Data/'
+    TRAINING_DIR = 'Processed Data/CE_PREPROC/train/'
+    TESTING_DIR = 'Processed Data/CE_PREPROC/test/'
+    # os.makedirs(os.path.join(TRAINING_DIR, 'IMGs'), exist_ok=True)
+    # os.makedirs(os.path.join(TRAINING_DIR, 'masks'), exist_ok=True)
+    # os.makedirs(os.path.join(TESTING_DIR, 'IMGs'), exist_ok=True)
+    # os.makedirs(os.path.join(TESTING_DIR, 'masks'), exist_ok=True)
+
+    if len(os.listdir(TRAINING_DIR+'IMGs')) + len(os.listdir(TESTING_DIR+'IMGs')) == 0:
+        for file in tqdm.tqdm(os.listdir(DATASOURCE)):
+            split = np.random.random()
+            img = cv2.imread(os.path.join(DATASOURCE, file), cv2.IMREAD_GRAYSCALE)
+            img = img[20:-20,:] # Images contain significant noise near top and bottom that couldn't be removed classically
+            binary_mask = generate_binary_mask(img)
+            if split < 0.8:
+                # Training Data
+                img = increase_contrast(img, grid_size=(15,15))
+                _, cc_mask = cv2.connectedComponents(binary_mask)
+                cv2.imwrite(os.path.join(TRAINING_DIR,'IMGs',file[-10:-4]+'.png'), img)
+                cv2.imwrite(os.path.join(TRAINING_DIR,'masks',file[-10:-4]+'.png'), cc_mask)
+                i = 1
+                while i < 5:
+                    aug = False
+                    aug_img, aug_mask = img, binary_mask
+                    if np.random.random() > 0.5:
+                        aug = True
+                        aug_img, aug_mask = rand_crop(aug_img, aug_mask)
+                    if np.random.random() > 0.5:
+                        aug = True
+                        aug_img, aug_mask = rand_flip(aug_img, aug_mask)
+                    if aug:
+                        _, aug_mask = cv2.connectedComponents(aug_mask)
+                        cv2.imwrite(os.path.join(TRAINING_DIR,'IMGs',f'{file[-10:-4]}_{i}.png'), aug_img)
+                        cv2.imwrite(os.path.join(TRAINING_DIR,'masks',f'{file[-10:-4]}_{i}.png'), aug_mask)
+                        i+=1
+            else:
+                # Testing Data
+                _, cc_mask = cv2.connectedComponents(binary_mask)
+                img = increase_contrast(img, grid_size=(15,15))
+
+                cv2.imwrite(os.path.join(TESTING_DIR,'IMGs',file[-10:-4]+'.png'), img)
+                cv2.imwrite(os.path.join(TESTING_DIR,'masks',file[-10:-4]+'.png'), cc_mask)
+    else:
+        print(f'{len(os.listdir(TRAINING_DIR+"IMGs"))=}')
+        print(f'{len(os.listdir(TESTING_DIR+"IMGs"))=}')
 
